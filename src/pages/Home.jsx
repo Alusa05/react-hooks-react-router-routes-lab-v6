f27448d2a80512c1b25b4af7513fdbc9a7981239
Lisa Alusa <sayilisaalusa@gmail.com>
2025-04-23 12:02:28 +0300

    2 modifications

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,24 @@
 import { useEffect, useState } from "react";
-
+import MovieCard from "../components/MovieCard";
+import NavBar from "../components/NavBar";
 function Home() {
-  return (
-    <>
-      <header>
-        {/* What component should go here? */}
-      </header>
-      <main>
-        {/* Info goes here! */}
-      </main>
-    </>
-  );
-};
+  const [movies, setMovies] = useState([]);
+  
+  useEffect(() => {
+ fetch("http://localhost:3000/movies")
+  .then((response) => response.json())
+  .then(data => setMovies(data))
+  .catch(error => console.error(error));
+}, []);
+return (
+  <>
+  <NavBar />
+  <h1>Home Page</h1>
+  {movies.map(movie => (
+    <MovieCard key ={movie.id} movie={movie} />
+  ))}
+  </>
+);
+}
 
 export default Home;
